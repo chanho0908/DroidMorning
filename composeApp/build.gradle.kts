b90f11f20dde944dfc1284f56e9f57dcb6ae7e71
chanho0908 <uni070@naver.com>
2026-01-07 23:02:04 +0900

    feat: Napier 초기화 로직 공통 모듈로 분리

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -107,6 +107,9 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        buildConfig = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
