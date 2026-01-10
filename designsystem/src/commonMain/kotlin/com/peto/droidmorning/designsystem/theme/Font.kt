adf0740a543dc2c3170680c4ddda032858b21bcd
chanho0908 <uni070@naver.com>
2026-01-11 00:52:40 +0900

    refactor: 리소스 클래스 이름 변경에 따른 import 경로 수정

--- designsystem/src/commonMain/kotlin/com/peto/droidmorning/designsystem/theme/Font.kt
+++ designsystem/src/commonMain/kotlin/com/peto/droidmorning/designsystem/theme/Font.kt
@@ -3,29 +3,29 @@ package com.peto.droidmorning.designsystem.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import droidmorning.designsystem.generated.resources.Res
-import droidmorning.designsystem.generated.resources.pretendard_black
-import droidmorning.designsystem.generated.resources.pretendard_bold
-import droidmorning.designsystem.generated.resources.pretendard_extrabold
-import droidmorning.designsystem.generated.resources.pretendard_extralight
-import droidmorning.designsystem.generated.resources.pretendard_light
-import droidmorning.designsystem.generated.resources.pretendard_medium
-import droidmorning.designsystem.generated.resources.pretendard_regular
-import droidmorning.designsystem.generated.resources.pretendard_semibold
-import droidmorning.designsystem.generated.resources.pretendard_thin
+import com.peto.droidmorning.designsystem.generated.resources.DesignRes
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_black
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_bold
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_extrabold
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_extralight
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_light
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_medium
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_regular
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_semibold
+import com.peto.droidmorning.designsystem.generated.resources.pretendard_thin
 import org.jetbrains.compose.resources.Font
 
 val droidMorningFontFamily: FontFamily
     @Composable
     get() =
         FontFamily(
-            Font(Res.font.pretendard_thin, FontWeight.Thin),
-            Font(Res.font.pretendard_extralight, FontWeight.ExtraLight),
-            Font(Res.font.pretendard_light, FontWeight.Light),
-            Font(Res.font.pretendard_regular, FontWeight.Normal),
-            Font(Res.font.pretendard_medium, FontWeight.Medium),
-            Font(Res.font.pretendard_semibold, FontWeight.SemiBold),
-            Font(Res.font.pretendard_bold, FontWeight.Bold),
-            Font(Res.font.pretendard_extrabold, FontWeight.ExtraBold),
-            Font(Res.font.pretendard_black, FontWeight.Black),
+            Font(DesignRes.font.pretendard_thin, FontWeight.Thin),
+            Font(DesignRes.font.pretendard_extralight, FontWeight.ExtraLight),
+            Font(DesignRes.font.pretendard_light, FontWeight.Light),
+            Font(DesignRes.font.pretendard_regular, FontWeight.Normal),
+            Font(DesignRes.font.pretendard_medium, FontWeight.Medium),
+            Font(DesignRes.font.pretendard_semibold, FontWeight.SemiBold),
+            Font(DesignRes.font.pretendard_bold, FontWeight.Bold),
+            Font(DesignRes.font.pretendard_extrabold, FontWeight.ExtraBold),
+            Font(DesignRes.font.pretendard_black, FontWeight.Black),
         )
