2678a2e169f69932d68a4bf362806ab67563888e
chanho0908 <uni070@naver.com>
2026-01-12 01:24:31 +0900

    refactor: MainScreen에서 interactionSource 중복 생성 방지

--- composeApp/src/commonMain/kotlin/com/peto/droidmorning/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/peto/droidmorning/main/MainScreen.kt
@@ -28,6 +28,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun MainScreen() {
     var selectedTab by remember { mutableStateOf(BottomNavigationType.QUESTION) }
+    val interactionSource = remember { MutableInteractionSource() }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -58,7 +59,7 @@ fun MainScreen() {
                                 unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
                                 unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
                             ),
-                        interactionSource = MutableInteractionSource(),
+                        interactionSource = interactionSource,
                     )
                 }
             }
